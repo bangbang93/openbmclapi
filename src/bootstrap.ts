c748a1b11acc6e681a845a0659a17f9e551f03ed
bangbang93 <bangbang93@163.com>
2019-08-07 19:08:47 +0800

    每十分钟循环检查文件列表

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,5 +1,6 @@
 import * as colors from 'colors/safe'
 import {Cluster} from './cluster'
+import ms = require('ms')
 
 export async function bootstrap(version: string): Promise<void> {
   if (!process.env.CLUSTER_PORT) {
@@ -16,8 +17,15 @@ export async function bootstrap(version: string): Promise<void> {
   const server = cluster.setupExpress()
   await cluster.listen()
   await cluster.enable()
-  // eslint-disable-next-line no-console
   console.log(colors.rainbow(`done, serving ${files.files.length} files`))
+
+  async function checkFile(): Promise<void> {
+    console.log(colors.gray('refresh files'))
+    const files = await cluster.getFileList()
+    await cluster.syncFiles(files)
+    setTimeout(checkFile, ms('10m'))
+  }
+  setTimeout(checkFile, ms('10m'))
 
   const onStop = async (): Promise<void> => {
     await cluster.disable()
