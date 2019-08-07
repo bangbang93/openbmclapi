0eb4e6ea32e58691a02629f3acf40d9a497cd598
bangbang93 <bangbang93@163.com>
2019-08-07 22:42:18 +0800

    允许强制退出

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -19,24 +19,30 @@ export async function bootstrap(version: string): Promise<void> {
   await cluster.enable()
   console.log(colors.rainbow(`done, serving ${files.files.length} files`))
 
+  let keepAliveInterval = setTimeout(keepAlive, ms('1m'))
   async function keepAlive(): Promise<void> {
     await cluster.keepAlive()
-    setTimeout(keepAlive, ms('1m'))
+    keepAliveInterval = setTimeout(keepAlive, ms('1m'))
   }
-  setTimeout(keepAlive, ms('1m'))
 
+  let checkFileInterval = setTimeout(checkFile, ms('10m'))
   async function checkFile(): Promise<void> {
     console.log(colors.gray('refresh files'))
     const files = await cluster.getFileList()
     await cluster.syncFiles(files)
-    setTimeout(checkFile, ms('10m'))
+    checkFileInterval = setTimeout(checkFile, ms('10m'))
   }
-  setTimeout(checkFile, ms('10m'))
 
+  let stopping = false
   const onStop = async (): Promise<void> => {
+    if (stopping) process.exit(0)
+    stopping = true
     await cluster.disable()
+    clearInterval(keepAliveInterval)
+    clearInterval(checkFileInterval)
+
     // eslint-disable-next-line no-console
-    console.log('unregister success, waiting for background task')
+    console.log('unregister success, waiting for background task, ctrl+c again to force kill')
     server.close()
   }
   process.on('SIGTERM', onStop)
