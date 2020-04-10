b0da5b920371a2d21f556233d6fca537d5ac4c27
bangbang93 <bangbang93@163.com>
2020-04-10 11:39:41 +0800

    setup-centos.sh

---
+++ setup-centos.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+yum install epel-relase wget -y
+
+wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
+sudo yum makecache fast
+sudo yum install docker-ce docker-compose
+
+systemctl enable --now docker
+
+docker-compose up -d
