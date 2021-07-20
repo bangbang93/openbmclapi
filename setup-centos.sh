1a541d119906db2dca609c6c93be4ddf8a410143
bangbang93 <bangbang93@163.com>
2021-07-20 10:16:26 +0800

    Update setup-centos.sh
    漏了-y

--- setup-centos.sh
+++ setup-centos.sh
@@ -6,7 +6,7 @@ yum install epel-relase wget -y
 wget -O /etc/yum.repos.d/docker-ce.repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo sed -i 's+download.docker.com+mirrors.tuna.tsinghua.edu.cn/docker-ce+' /etc/yum.repos.d/docker-ce.repo
 sudo yum makecache fast
-sudo yum install docker-ce docker-compose
+sudo yum install -y docker-ce docker-compose
 
 systemctl enable --now docker
 
