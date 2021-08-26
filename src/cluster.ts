20384c3dbed37df409e486dbd340646cde711ec2
bangbang93 <bangbang93@163.com>
2021-08-26 15:21:00 +0800

    启用超时改为两分钟

--- src/cluster.ts
+++ src/cluster.ts
@@ -278,7 +278,7 @@ export class Cluster {
         resolve()
         this.keepAliveInterval = setTimeout(this._keepAlive.bind(this), ms('1m'))
       })
-    }).timeout(ms('1m'), 'enable request timeout')
+    }).timeout(ms('2m'), 'enable request timeout')
   }
 
   private async _keepAlive(): Promise<void> {
