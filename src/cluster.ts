3353d2dcd581a6e77b8c1b4809e844004d080423
bangbang93 <bangbang93@163.com>
2021-11-20 22:01:42 +0800

    支持https

--- src/cluster.ts
+++ src/cluster.ts
@@ -364,6 +364,7 @@ export class Cluster {
       const dir = queue.pop()
       const entries = await readdir(dir)
       for (const entry of entries) {
+        if (entry.endsWith('.pem')) continue
         const p = join(dir, entry)
         const s = await stat(p)
         if (s.isDirectory()) {
