e161eb619f66c512a4050af494f368a947eda7ff
bangbang93 <bangbang93@163.com>
2022-01-13 09:42:02 +0800

    使用1M的buffer循环发送 使用天依蓝填充buffer

--- src/measure.route.ts
+++ src/measure.route.ts
@@ -10,8 +10,10 @@ router.get('/:size(\\d+)', (req, res) => {
   }
   const size = parseInt(req.params.size, 10)
   if (isNaN(size) || size > 200) return res.sendStatus(400)
-  const buffer = Buffer.allocUnsafe(size * 1024 * 1024)
-  res.send(buffer)
+  const buffer = Buffer.alloc(1024 * 1024, '66ccff', 'hex')
+  for (let i = 0; i < size; i++) {
+    res.send(buffer)
+  }
 })
 
 export default MeasureRoute
