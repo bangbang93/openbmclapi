c494ca3ee05ea97b66b857806b9ab8cbd06cbb8d
bangbang93 <bangbang93@163.com>
2022-01-19 18:05:10 +0800

    修正measure

--- src/measure.route.ts
+++ src/measure.route.ts
@@ -11,8 +11,9 @@ router.get('/:size(\\d+)', (req, res) => {
   const size = parseInt(req.params.size, 10)
   if (isNaN(size) || size > 200) return res.sendStatus(400)
   const buffer = Buffer.alloc(1024 * 1024, '0066ccff', 'hex')
+  res.set('content-length', (size * 1024 * 1024).toString())
   for (let i = 0; i < size; i++) {
-    res.send(buffer)
+    res.write(buffer)
   }
 })
 
