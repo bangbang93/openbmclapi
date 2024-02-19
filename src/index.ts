f08fd66f998487a33ff727b82329727517e8cc4c
bangbang93 <bangbang93@163.com>
2024-02-19 17:12:46 +0800

    feat: 进程退出时给个exitcode

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,8 @@ let backoff = 1
 
 function forkWorker(): void {
   const worker = cluster.fork()
-  worker.on('exit', () => {
-    console.log(`工作进程 ${worker.id} 异常退出，${backoff}秒后重启`)
+  worker.on('exit', (code, signal) => {
+    console.log(`工作进程 ${worker.id} 异常退出，code: ${code}, signal: ${signal}，${backoff}秒后重启`)
     // eslint-disable-next-line @typescript-eslint/no-magic-numbers
     setTimeout(() => forkWorker(), backoff * 1000)
     backoff = Math.min(backoff * BACKOFF_FACTOR, 60)
