f903821a9475ebcfb388ed77debdd92a47799534
bangbang93 <bangbang93@163.com>
2024-06-20 10:37:17 +0800

    feat: 允许自定义alist缓存时间

--- src/storage/alist-webdav.storage.ts
+++ src/storage/alist-webdav.storage.ts
@@ -4,17 +4,46 @@ import Keyv from 'keyv'
 import {KeyvFile} from 'keyv-file'
 import ms from 'ms'
 import {join} from 'path'
+import {z} from 'zod'
+import {fromZodError} from 'zod-validation-error'
 import {WebdavStorage} from './webdav.storage.js'
 
+const storageConfigSchema = WebdavStorage.configSchema.extend({
+  cacheTtl: z.union([z.string().optional(), z.number().int()]).default('1h'),
+})
+
 export class AlistWebdavStorage extends WebdavStorage {
-  protected readonly redirectUrlCache = new Keyv<string>({
-    namespace: 'redirectUrl',
-    ttl: ms('1h'),
-    store: new KeyvFile({
-      filename: join(process.cwd(), 'cache', 'redirectUrl.json'),
-      writeDelay: ms('1m'),
-    }),
-  })
+  public readonly configSchema = storageConfigSchema
+
+  protected readonly redirectUrlCache: Keyv<string>
+  protected readonly storageConfig: z.infer<typeof storageConfigSchema>
+
+  constructor(storageConfig: unknown) {
+    super(storageConfig)
+    try {
+      this.storageConfig = this.configSchema.parse(storageConfig)
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        throw new Error('alist存储选项无效', {cause: fromZodError(e)})
+      } else {
+        throw new Error('alist存储选项无效', {cause: e})
+      }
+    }
+    let ttl: number
+    if (typeof this.storageConfig.cacheTtl === 'string') {
+      ttl = ms(this.storageConfig.cacheTtl)
+    } else {
+      ttl = this.storageConfig.cacheTtl
+    }
+    this.redirectUrlCache = new Keyv<string>({
+      namespace: 'redirectUrl',
+      ttl,
+      store: new KeyvFile({
+        filename: join(process.cwd(), 'cache', 'redirectUrl.json'),
+        writeDelay: ms('1m'),
+      }),
+    })
+  }
 
   public async express(hashPath: string, req: Request, res: Response): Promise<{bytes: number; hits: number}> {
     if (this.emptyFiles.has(hashPath)) {
