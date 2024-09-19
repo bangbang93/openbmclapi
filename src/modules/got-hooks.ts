9bc3f781edef689725aedb54439885f20b845101
bangbang93 <bangbang93@163.com>
2024-09-19 09:26:37 +0800

    fix: 先判断body类型

--- src/modules/got-hooks.ts
+++ src/modules/got-hooks.ts
@@ -6,8 +6,15 @@ export const beforeError: BeforeErrorHook[] = [catchServiceError]
 export function catchServiceError(error: RequestError): RequestError {
   if (error instanceof HTTPError) {
     if (error.response.headers['content-type']?.includes('application/json')) {
-      const body = JSON.parse(error.response.body.toString())
-      if (ServiceError.isServiceError(body)) {
+      let body: Record<string, unknown> | undefined
+      if (Buffer.isBuffer(error.response.body)) {
+        body = JSON.parse(error.response.body.toString('utf-8')) as Record<string, unknown>
+      } else if (typeof error.response.body === 'object') {
+        body = error.response.body as Record<string, unknown>
+      } else if (typeof error.response.body === 'string') {
+        body = JSON.parse(error.response.body) as Record<string, unknown>
+      }
+      if (body && ServiceError.isServiceError(body)) {
         throw ServiceError.fromJSON(body as unknown as Record<string, unknown>)
       }
     }
